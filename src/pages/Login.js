22a31c287fef4344275a11a0d72f5bf454318d99
pamuditha <pamudithasenanayake@gmail.com>
2025-08-08 11:25:53 +0530

    updated never used links

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Login.css'; // Don't forget to import the CSS file!
 
 function Login() {
--- src/pages/matrix.js
+++ src/pages/matrix.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
 import EisenhowerMatrix from './EisenhowerMatrix';
-import styles from './matrix.css';
+
 
 const DashboardPage = () => {
     return (
